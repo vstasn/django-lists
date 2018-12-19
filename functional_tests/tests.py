214d433768013645b28d4810a188b6c8872bdbef
vstasn <vstasn@gmail.com>
2018-12-20 00:58:22 +0500

    Use Bootstrap for improving maq

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 5
+MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     '''test new user'''
 
     def setUp(self):
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         '''test layout and styling'''
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(900, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('testing')
@@ -105,6 +105,6 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            450,
             delta=10
         )
