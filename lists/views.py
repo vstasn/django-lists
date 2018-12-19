8c2c6c4e12f2f956d53d04cae2f6dab347e93356
vstasn <vstasn@gmail.com>
2018-12-19 23:45:22 +0500

    New URL + view for adding to existing lists. FT passes

--- lists/views.py
+++ lists/views.py
@@ -6,14 +6,21 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
+def view_list(request, list_id):
     '''view of list'''
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     '''new list'''
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/one-in-the-world-list/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    '''add item'''
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
