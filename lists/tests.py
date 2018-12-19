975ba2626d0fad1fb2f6e0c7ba50424aeccfbc4b
vstasn <vstasn@gmail.com>
2018-12-19 20:23:09 +0500

    Redirection after POST and show all elements in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from .models import Item
 
 
 class ItemModelTest(TestCase):
@@ -32,6 +32,29 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         '''test: possibility to save post-request'''
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        '''test: redirect after post-request'''
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        '''test: save elements when it is necessary'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        '''test: show all elements of list'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
