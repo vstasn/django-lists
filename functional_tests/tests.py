c6b715578a4e574f67a57755d127e206ab2726b7
vstasn <vstasn@gmail.com>
2018-12-20 14:06:19 +0500

    Add gunicorn for virtualenv

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
