a404e8318e6c5cfc6e410508d811d7c1160329f4
vstasn <vstasn@gmail.com>
2018-12-21 12:08:04 +0500

    Validation duplicate element in model

--- lists/models.py
+++ lists/models.py
@@ -12,3 +12,10 @@ class Item(models.Model):
     '''element of list'''
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
