ec8f9fcb47d4f902e373138266f9e9e484466177
vstasn <vstasn@gmail.com>
2018-12-22 21:34:33 +0500

    Move qunit.js to cdn

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LogoutView
 from accounts import views
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
+    url(r'^logout$', LogoutView.as_view(template_name="base.html"), name='logout'),
 ]
